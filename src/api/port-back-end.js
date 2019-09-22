5950d64a80d0f73f4eebf0cf572468dbf7ae0e36
tongznr <apisitk10@gmail.com>
2019-09-22 11:58:10 +0700

    update api url

--- src/api/port-back-end.js
+++ src/api/port-back-end.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = process.env.NODE_ENV === 'production' ? 'https://evening-dawn-14923.herokuapp.com' : 'http://localhost:3000';
+export const baseURL = process.env.NODE_ENV === 'production' ? 'https://tong-app-back.herokuapp.com' : 'http://localhost:3000';
 export const urlUsersImage = baseURL + '/img/users'
 
 export default axios.create({
